735ab5f4342ea1f2bdb48bb046dbb08a630d96db
tabatak <hamamoto.kazuto@gmail.com>
2018-10-29 11:15:13 +0900

    deadlin check

--- concurrency-in-go/context-package/main.go
+++ concurrency-in-go/context-package/main.go
@@ -76,6 +76,13 @@ func genFarewell(ctx context.Context) (string, error) {
 }
 
 func locale(ctx context.Context) (string, error) {
+	if deadline, ok := ctx.Deadline(); ok {
+		fmt.Printf("ctx.Deadline(): %v\n", deadline)
+		if deadline.Sub(time.Now().Add(1*time.Minute)) <= 0 {
+			return "", context.DeadlineExceeded
+		}
+	}
+
 	select {
 	case <-ctx.Done():
 		return "", ctx.Err()
